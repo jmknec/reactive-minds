0e69c73a2051258a481566fd5216430f1ce175bf
Jenna Knechtel <jmknec@gmail.com>
2024-07-02 11:32:47 -0400

    set up get route for strategies by emotional state

--- reactive-minds-server/routes/strategies.js
+++ reactive-minds-server/routes/strategies.js
@@ -4,11 +4,25 @@ import configuration from "../knexfile.js";
 
 const router = express.Router();
 const knex = initKnex(configuration);
-
+//get global list of strategies
 router.route("/").get(async (_req, res) => {
   try {
     const listStrategies = await knex("strategies");
     res.json(listStrategies);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Error getting strategies");
+  }
+});
+//get list of strategies by emotional state
+router.route("/:state").get(async (req, res) => {
+  const state = req.params.state;
+  try {
+    const stateStrategies = await knex
+      .select("*")
+      .from("strategies")
+      .where("emotional_state", state);
+    res.json(stateStrategies);
   } catch (error) {
     console.error(error);
     return res.status(500).send("Error getting strategies");
