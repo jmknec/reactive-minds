e765125eca6cf589a07acd2de129b919a7b75f0a
Jenna Knechtel <jmknec@gmail.com>
2024-07-03 09:56:09 -0400

    set up post route for user log in

--- reactive-minds-server/routes/users.js
+++ reactive-minds-server/routes/users.js
@@ -26,9 +26,30 @@ router.route("/register").post(async (req, res) => {
       return res.status(400).send("User already exists. Please sign in.");
     }
     await knex("users").insert(newUser);
-    res.status(201).json({ message: "User added successfully", user: newUser });
+    res.status(201).send("User added successfully! You can now log in.");
   } catch {
     res.status(500).send("Error registering new user");
+  }
+});
+//log into existing account
+router.route("/login").post(async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await knex.select("*").from("users").where("email", email);
+    if (user.length == 0) {
+      return res
+        .status(404)
+        .send("User does not exist. Please register for an account.");
+    }
+    const password_hash = user[0].password_hash;
+    if (await bcrypt.compare(password, password_hash)) {
+      return res.send(`Successfully logged in. Welcome, ${user[0].username}!`);
+    }
+    res.send("Incorrect password.");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error logging in.");
   }
 });
 
