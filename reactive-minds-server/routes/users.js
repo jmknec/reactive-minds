5a77815c496a16e504c14389bdd20b560587c663
Jenna Knechtel <jmknec@gmail.com>
2024-07-03 11:53:01 -0400

    implement JWTs on successful login

--- reactive-minds-server/routes/users.js
+++ reactive-minds-server/routes/users.js
@@ -2,10 +2,19 @@ import express from "express";
 import initKnex from "knex";
 import configuration from "../knexfile.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 const knex = initKnex(configuration);
+const jsonSecretKey = process.env.SECRET_KEY;
 
+function getToken(req) {
+  if (!req.headers.authorization) {
+    return;
+  } else {
+    return req.headers.authorization.split(" ")[1];
+  }
+}
 //register new user
 router.route("/register").post(async (req, res) => {
   const { email, username, password } = req.body;
@@ -34,22 +43,30 @@ router.route("/register").post(async (req, res) => {
 //log into existing account
 router.route("/login").post(async (req, res) => {
   const { email, password } = req.body;
+  const token = getToken(req);
 
   try {
     const user = await knex.select("*").from("users").where("email", email);
     if (user.length == 0) {
-      return res
-        .status(404)
-        .send("User does not exist. Please register for an account.");
+      return res.status(404).json({
+        token: "",
+        message: "User does not exist. Please register for an account.",
+      });
     }
     const password_hash = user[0].password_hash;
     if (await bcrypt.compare(password, password_hash)) {
-      return res.send(`Successfully logged in. Welcome, ${user[0].username}!`);
+      return res.json({
+        token: jwt.sign({ username: user.username }, jsonSecretKey),
+        message: `Successfully logged in. Welcome, ${user[0].username}!`,
+      });
     }
-    res.send("Incorrect password.");
+    res.status(403).json({
+      token: "",
+      message: "Incorrect password.",
+    });
   } catch (error) {
     console.error(error);
-    res.status(500).send("Error logging in.");
+    res.status(500).json({ token: "", message: "Error logging in." });
   }
 });
 
