3f6f93fc6434c113829989bdb26562d5d8df53f4
Jenna Knechtel <jmknec@gmail.com>
2024-07-04 15:47:36 -0400

    add front-end API call for list of given user's strategies

--- reactive-minds-client/src/components/UserStrategies/UserStrategies.jsx
+++ reactive-minds-client/src/components/UserStrategies/UserStrategies.jsx
@@ -1,14 +1,28 @@
 import { useContext } from "react";
+import axios from "axios";
 import "./UserStrategies.scss";
 import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 const baseUrl = import.meta.env.VITE_API_URL;
 
 export default function UserStrategies() {
   const { currentUser } = useContext(CurrentUserContext);
-  const { userId } = useParams;
-  const stratUrl = `${baseUrl}/${userId}/strategies`;
+  const userId = currentUser.id;
+  const stratUrl = `${baseUrl}/users/${userId}/strategies`;
+
+  useEffect(() => {
+    const getUserStrategies = async () => {
+      try {
+        const userStratResponse = await axios.get(`${stratUrl}`);
+        console.log(userStratResponse.data);
+      } catch (error) {
+        console.error("Error fetching user's strategies");
+      }
+    };
+    getUserStrategies();
+  }, []);
 
   return (
     <>
