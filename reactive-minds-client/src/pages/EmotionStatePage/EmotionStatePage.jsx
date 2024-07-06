9b50170dc86f2524866d4eb7045a2889f71a33bc
Jenna Knechtel <jmknec@gmail.com>
2024-07-06 10:47:17 -0400

    refactor styles for reusability across pages

--- reactive-minds-client/src/pages/EmotionStatePage/EmotionStatePage.jsx
+++ reactive-minds-client/src/pages/EmotionStatePage/EmotionStatePage.jsx
@@ -5,9 +5,11 @@ export default function EmotionStatePage() {
   return (
     <main className="page page--states">
       <HeroBanner title="Emotion Regulation" />
-      <section className="intro">
-        <h2 className="intro__heading">What is Emotion Regulation?</h2>
-        <p className="intro__description">
+      <section className="text text--states">
+        <h2 className="text__heading text__heading--states">
+          What is Emotion Regulation?
+        </h2>
+        <p className="text__description text__description--states">
           Emotion regulation is the ability to effectively manage and respond to
           an emotional experience. Think of it as a scale where the midpoint
           represents optimal emotion regulation. In this state, individuals are
@@ -15,23 +17,27 @@ export default function EmotionStatePage() {
           healthy and adaptive manner. They can maintain a balance where
           emotions neither overwhelm nor diminish their ability to function.
         </p>
-        <h3 className="intro__heading">Understanding Dysregulation</h3>
-        <p className="intro__description">
+        <h3 className="text__heading text__heading--states">
+          Understanding Dysregulation
+        </h3>
+        <p className="text__description text__description--states">
           On one end of the scale lies hypoarousal or low reactivity. Here,
           individuals may struggle with emotional numbness or difficulty
           engaging with their feelings. This state can manifest as withdrawal,
           lethargy, or disconnection from emotions, making it challenging to
           respond appropriately to emotional cues.
         </p>
-        <p className="intro__description">
+        <p className="text__description text__description--states">
           On the opposite end of the spectrum is hyperarousal or high
           reactivity. In this state, emotions are intense and overwhelming,
           often leading to impulsive reactions or emotional outbursts.
           Individuals may feel easily agitated, anxious, or unable to calm down,
           which can interfere with their daily lives and relationships.
         </p>
-        <h3 className="intro__heading">The Spectrum of Emotion Regulation</h3>
-        <p className="intro__description">
+        <h3 className="text__heading text__heading--states">
+          The Spectrum of Emotion Regulation
+        </h3>
+        <p className="text__description text__description--states">
           Imagine this spectrum as a journey from balanced emotion regulation at
           the midpoint to extremes of hypoarousal and hyperarousal at either
           end. Understanding where an individual falls on this spectrum can
@@ -40,7 +46,7 @@ export default function EmotionStatePage() {
           help individuals, especially children, move towards the center of the
           scale where emotions are managed effectively and constructively.
         </p>
-        <p className="intro__description">
+        <p className="text__description text__description--states">
           By exploring this spectrum, caregivers can gain a deeper understanding
           of their child's emotional challenges and tailor interventions to
           support them in achieving greater emotional well-being and
--- reactive-minds-client/src/pages/HomePage/HomePage.jsx
+++ reactive-minds-client/src/pages/HomePage/HomePage.jsx
@@ -5,14 +5,15 @@ export default function HomePage() {
   return (
     <main className="page page--home">
       <HeroBanner title="Welcome to Reactive Minds" />
-      <div className="about">
-        <p className="about__description">
+      <section className="text text--home">
+        <h2 className="text__heading text__heading--home">Who are we?</h2>
+        <p className="text__description text__description--home">
           We're here to support parents and caregivers of children experiencing
           emotion dysregulation. You'll find valuable resources and techniques
           designed to empower you in nurturing your child's emotional
           well-being.
         </p>
-        <p className="about__description">
+        <p className="text__description text__description--home">
           Whether your child is navigating typical developmental challenges or
           faces additional hurdles due to neurodivergence or mental health
           disorders, our goal is to provide practical strategies tailored to
@@ -20,7 +21,7 @@ export default function HomePage() {
           to stimulating play-based activities for periods of low reactivity,
           we're here to help you every step of the way.
         </p>
-        <p className="about__description">
+        <p className="text__description text__description--home">
           Learn more about emotion dysregulation right off the bat, or register
           for an account where you can personalize your experience by saving
           your favorite techniques and tracking their effectiveness over time.
@@ -28,7 +29,7 @@ export default function HomePage() {
           emotionally. Join our community and embark on this journey towards
           greater understanding and support.
         </p>
-      </div>
+      </section>
     </main>
   );
 }
