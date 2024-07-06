1baabefd1ed2e3cf10f2d3f921a6abb9f655710b
Jenna Knechtel <jmknec@gmail.com>
2024-07-06 01:52:20 -0400

    add large screen styles footer component

--- reactive-minds-client/src/components/Footer/Footer.jsx
+++ reactive-minds-client/src/components/Footer/Footer.jsx
@@ -4,18 +4,20 @@ import "./Footer.scss";
 export default function Footer({ icon, logo }) {
   return (
     <footer className="footer">
-      <span className="footer__dev">Coded by J Knechtel</span>
-      <div className="footer__logo-container">
-        <Logo
-          logoClass="footer__logo"
-          src={logo}
-          altText="scribbled illustration of a brain with words 'Reactive Minds'"
-        />
-        <Logo
-          logoClass="footer__icon"
-          src={icon}
-          altText="scribbled illustration of a brain"
-        />
+      <div className="footer__container">
+        <span className="footer__dev">Coded by J Knechtel</span>
+        <div className="footer__logo-container">
+          <Logo
+            logoClass="footer__logo"
+            src={logo}
+            altText="scribbled illustration of a brain with words 'Reactive Minds'"
+          />
+          <Logo
+            logoClass="footer__icon"
+            src={icon}
+            altText="scribbled illustration of a brain"
+          />
+        </div>
       </div>
     </footer>
   );
