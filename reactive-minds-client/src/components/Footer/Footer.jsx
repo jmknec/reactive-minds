1dd61016c5cdb76c29909502743829e4a54dd0aa
Jenna Knechtel <jmknec@gmail.com>
2024-07-06 01:18:01 -0400

    add mobile styles footer

--- reactive-minds-client/src/components/Footer/Footer.jsx
+++ reactive-minds-client/src/components/Footer/Footer.jsx
@@ -1,11 +1,21 @@
+import Logo from "../Logos/Logos";
 import "./Footer.scss";
 
 export default function Footer({ icon, logo }) {
   return (
     <footer className="footer">
-      <div className="footer__">
-        <img className="footer__icon" src={icon} alt="" />
-        <img className="footer__logo" src={logo} alt="" />
+      <span className="footer__dev">Coded by J Knechtel</span>
+      <div className="footer__logo-container">
+        <Logo
+          logoClass="footer__logo"
+          src={logo}
+          altText="scribbled illustration of a brain with words 'Reactive Minds'"
+        />
+        <Logo
+          logoClass="footer__icon"
+          src={icon}
+          altText="scribbled illustration of a brain"
+        />
       </div>
     </footer>
   );
